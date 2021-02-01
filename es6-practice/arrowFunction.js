ce7e891bcb95dc1d07e94ad8fe18a217446f7ac2
Kamrul Hasan <kamrulhasanit5@gmail.com>
2021-02-01 18:13:48 +0600

    added arrowFunction.js code

---
+++ es6-practice/arrowFunction.js
@@ -0,0 +1,69 @@
+// function doubleIt(num){
+//     return num * 2;
+// }
+// const result = doubleIt(15);
+// console.log(result);
+
+
+// const doubleIt = function(num){
+//     return num * 2;
+// }
+// const result = doubleIt(20);
+// console.log(result);
+
+// const doubleIt = num => num * 2;
+// const result = doubleIt(23);
+// console.log(result);
+
+// const add = (x, y) => x + y;
+// const result = add(50, 70);
+// console.log(result);
+
+// const giveNumber = () => 7;
+// const result2 = giveNumber();
+// console.log(result2);
+
+// const doMath = (x, y) => {
+//     const sum = x + y;
+//     const diff = x - y;
+//     const result = sum * diff;
+//     return result;
+// }
+// const result3 = doMath(7, 5);
+// console.log(result3)
+
+
+
+// 2nd Time Practice
+function doubleIt(num){
+    return num * 2;
+};
+const result10 = doubleIt(5);
+console.log(result10);
+
+const doubleIt2 = function(num){
+    return num * 2;
+};
+const result11 = doubleIt2(7);
+console.log(result11);
+
+const doubleIt3 = num => num * 2;
+const result12 = doubleIt3(12);
+console.log(result12);
+
+const doubleIt4 = (x, y) => x + y;
+const result13 = doubleIt4(7, 5);
+console.log(result13);
+
+const doubleIt5 = () => 7;
+const result14 = doubleIt5();
+console.log(result14);
+
+const doubleIt6 = (x, y) => {
+    const sum = x + y;
+    const diff = x - y;
+    const result = sum * diff;
+    return result;
+};
+const result15 = doubleIt6(7, 5);
+console.log(result15);
