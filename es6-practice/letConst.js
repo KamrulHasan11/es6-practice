13964f096694a645013700321bb5be4a9cd7fa2c
Kamrul Hasan <kamrulhasanit5@gmail.com>
2021-02-01 16:02:35 +0600

    letConst.js code added

---
+++ es6-practice/letConst.js
@@ -0,0 +1,21 @@
+const hubby = "Rayhana Mariyam Toma";
+console.log(hubby);
+
+const numbers = [23, 45, 65, 34, 23];
+numbers[1] = 47;
+numbers.push(87);
+// numbers = ['Kamrul', 'Hasan', 'Niloy', 'Ahmed', 'Toma'];
+console.log(numbers);
+
+const publicFigure = {Name:'Solaiman Shukhon', Phone:9238747, Name:'Dr. Vivek Vindra', Phone:9283743};
+console.log(publicFigure);
+
+let booksName = 'JavaScript all in one';
+booksName = "You don't know js";
+console.log(booksName);
+
+let sum = 0;
+for (let i = 0; i <= 10; i++) {
+    sum = sum + i;
+    console.log(i);
+};
