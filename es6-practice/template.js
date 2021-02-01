e1af815076216d0c0bb2128d8feeef2ce157ac24
Kamrul Hasan <kamrulhasanit5@gmail.com>
2021-02-01 17:10:26 +0600

    added temlate.js fise

---
+++ es6-practice/template.js
@@ -0,0 +1,21 @@
+const firstName = 'Kamrul';
+const lastName = 'Hasan Niloy';
+const fullName = firstName + ' ' + lastName + ' is a good boy.';
+console.log(fullName);
+
+const firstName2 = 'Programmer';
+const lastName2 = 'Kamrul Hasan';
+const fullName2 = `${firstName2} ${lastName2} is a freelancer in Bangladesh.`;
+console.log(fullName2);
+
+// const multiLine = 'I love to code\n'
+//     +'I love to learn\n'
+//     +'I love to teach\n'
+//     +'I love to do hard-work';
+const multiLine = `Hello there,
+This is Programmer Kamrul Hasan
+is here to help you to solve your it and marketing related problem
+to increase your business world-wide.
+If you need any help just feel free to knock me.
+I am always ready to here your problem for solution.`
+console.log(multiLine);
